d07e2371259c975fbb0ad82fabd2aa381471284b
Renata Mendes <renatampc030802@gmail.com>
2021-05-17 10:53:41 -0300

    Criando controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,1 +1,21 @@
 #include "controller.h"
+
+void Controller::cadastraInsumosMinisterio(){
+
+}
+
+void Controller::distribuiInsumos(Local local, Insumo insumo){
+
+}
+
+void Controller::consultaInsumos(Local local){
+
+}
+
+void Controller::consultaInsumosDescricao(Local local){
+
+}
+
+void Controller::consultaTipoInsumos(Local local, int tipo){
+
+}
--- src/controller.h
+++ src/controller.h
@@ -1,9 +1,26 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "insumo.h"
+#include "local.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Controller {
+
+    private:
+        vector <Local> locais;
+
+    public:
+        void cadastraInsumosMinisterio(); // Cadastra insumos no MS
+
+        void distribuiInsumos(Local local, Insumo insumo); // Distribui insumos para estados
+
+        void consultaInsumos(Local local); // Consulta de insumos em determinado local
+        void consultaInsumosDescricao(Local local); // Consulta descrição de insumos de determinado local
+        void consultaTipoInsumos(Local local, int tipo); // Consulta de insumos por tipo
 
 };
 
